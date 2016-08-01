dcae69ed32b6f24b5a7d12ff74f85ddc48dfe3d6
Matt Turner <matthewallenturner@gmail.com>
2016-08-01 10:06:58 -0500

    smooth scrolling on anchor links

--- js/ace.js
+++ js/ace.js
@@ -4,4 +4,19 @@ $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
 
+// Smooth scrolling on anchor links
+$("#nav ul li a[href^='#']").on('click', function(e) {
+   // prevent default anchor click behavior
+   e.preventDefault();
+   // store hash
+   var hash = this.hash;
+   // animate
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top
+     }, 500, function(){
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
 
+});
