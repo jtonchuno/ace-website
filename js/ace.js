7c85570c962a32ea9828385072ffacd2416846c3
Matt Turner <matthewallenturner@gmail.com>
2016-08-01 15:06:15 -0500

    added carousel, updated nav bar colors

--- js/ace.js
+++ js/ace.js
@@ -20,3 +20,33 @@ $("#nav ul li a[href^='#']").on('click', function(e) {
      });
 
 });
+
+// Cache selectors
+var topMenu = $(".navbar-custom"),
+    topMenuHeight = topMenu.outerHeight()+15,
+    // All list items
+    menuItems = topMenu.find('a[href^="#"]'),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).attr("href"));
+      if (item.length) { return item; }
+    });
+
+// Bind to scroll
+$(window).scroll(function(){
+   // Get container scroll position
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+
+   // Get id of current scroll item
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+   // Get the id of the current element
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+   // Set/remove active class
+   menuItems
+     .parent().removeClass("active")
+     .end().filter("[href='#"+id+"']").parent().addClass("active");
+});
