badf215f89887a7898684559c1df987428aa7c7a
maxwmckinley <maxwmckinley@gmail.com>
2016-09-28 13:23:44 -0500

    Added validation that a uin was entered

--- member_info.php
+++ member_info.php
@@ -16,6 +16,7 @@ if(_POST)
 	$phone_number = $_POST['phone_number'];
 
 	// Data validation
+	if(!$uin) { die("UIN was not entered"); }
 	$joined = $joined . '-01';
 	$graduation = $graduation . '-01';
 
